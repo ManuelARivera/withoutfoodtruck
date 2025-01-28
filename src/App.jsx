ad418b71badb58d206ed4bc0560f7c14359fad98
Manuel A. Rivera <Manuel A. Rivera@DESKTOP-9E0ESCK>
2025-01-27 22:15:51 -0400

    se aplicó responsividad en .app_body

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { Stack, Autocomplete, TextField, Tooltip } from "@mui/material";
 import PlaceIcon from '@mui/icons-material/Place';
